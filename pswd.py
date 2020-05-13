ddd5aec3d28d120f8b7c4162d7357a99e557f12f
Project_Test <48741339+flying1574@users.noreply.github.com>
2020-05-13 22:53:31 +0800

    Update pswd.py

--- pswd.py
+++ pswd.py
@@ -2,10 +2,9 @@
 
 import os
 
-rar_path = "server.7z"
-with open('Decode.txt') as pswd:
+rar_path = "Package.7z" # The package.7z is Encrypted package Name 
+with open('Passwords.txt') as pswd:   #Password.txt is Encrypted package Password dictionary
     pw = pswd.readlines()
 for ps in pw:
     cmd = '7z.exe x "{}" -p{}'.format(rar_path,ps)
-    print(cmd)
-#    os.system(cmd)
+    os.system(cmd)
